3c2ecf1d429fbb700c7e16cb61483011edc3cc94
sebas-PC\sebas <osorno.sebastian@gmail.com>
2017-11-29 04:08:48 -0500

    Microservicio cliente,Final

--- src/main/java/com/udea/microserviceClient/MicroserviceClient.java
+++ src/main/java/com/udea/microserviceClient/MicroserviceClient.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -14,6 +16,7 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+
 public class MicroserviceClient {
 
 	public static void main(String[] args) {
